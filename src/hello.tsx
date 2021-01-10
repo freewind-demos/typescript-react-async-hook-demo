985c5d9c0902832686f7a7bfcedd5866730f0dc1
Freewind <nowindlee@gmail.com>
2021-01-10 15:06:31 +0800

    improve

--- src/hello.tsx
+++ src/hello.tsx
@@ -5,8 +5,8 @@ import fetchRemoteMessage from './fetchRemoteMessage';
 export default function Hello() {
   const result = useAsync(fetchRemoteMessage, ['RemoteHello1'])
   return <div>
-    {result?.loading && <div>Loading...</div>}
-    {result?.error && <div>Error: {result?.error?.message}</div>}
-    {result?.result !== undefined && <div>Hello, {result?.result}</div>}
+    {result.loading && <div>Loading...</div>}
+    {result.error && <div>Error: {result.error?.message}</div>}
+    {result.result !== undefined && <div>Hello, {result.result}</div>}
   </div>;
 };
